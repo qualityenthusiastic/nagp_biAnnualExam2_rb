df12db80bfc873295411080147aa69650faa9687
Raghav Bajoria <rbajoria@lutron.com>
2023-03-02 13:08:26 +0530

    code for devtestops

---
+++ src/test/java/TestCase1.java
@@ -0,0 +1,15 @@
+import baseClass.BaseClass;
+import org.testng.annotations.Test;
+
+public class TestCase1 extends BaseClass {
+    @Test
+    public void Testcase1(){
+        System.out.println("This is Test Case 1");
+    }
+
+    //testin
+    @Test
+    public void Testcase2(){
+        System.out.println("This is Test Case 2");
+    }
+}
